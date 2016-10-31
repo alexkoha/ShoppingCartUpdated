27d2ecbe526ec86b5f6371f17fd616a8063452ba
alexkoha <alexkoha@gmail.com>
2016-10-31 14:18:37 +0200

    Add files via upload

---
+++ ShoppingCart.WPF/Chain/ChainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.WPF.Chain
+{
+    public class ChainViewModel : ObservableObject
+    {
+        private string _imagePath;
+        private string _chainName;
+        private double _cartPrice;
+
+        private IEnumerable<Product> _expensiveProducts;
+        private IEnumerable<Product> _cheapestProducts;
+
+        public ChainViewModel(string chainName , string imagePath)
+        {
+            _chainName = chainName;
+            _imagePath = imagePath;
+            _cartPrice = 0;
+        }
+
+        public string ImagePath => _imagePath;
+        public string ChainName => _chainName;
+        public double CartPrice
+        {
+            get { return _cartPrice; }
+            set
+            {
+                if (Equals(value, _cartPrice)) return;
+                _cartPrice = value;
+                RaisePropertyChangedEvent("CartPrice");
+            }
+        }
+        public IEnumerable<Product> ExpensiveProducts
+        {
+            get { return _expensiveProducts; }
+            set
+            {
+                _expensiveProducts = value;
+                RaisePropertyChangedEvent("ExpensiveProducts");
+            }
+        }
+
+        public IEnumerable<Product> CheapestProducts
+        {
+            get { return _cheapestProducts; }
+            set
+            {
+                _cheapestProducts = value;
+                RaisePropertyChangedEvent("CheapestProducts");
+            }
+        }
+    }
+}
---
+++ ShoppingCart.WPF/MainMenu/MainMenuViewModel.cs
@@ -0,0 +1,55 @@
+﻿using ShoppingCart.WPF.Command;
+using ShoppingCart.WPF.Users;
+
+namespace ShoppingCart.WPF.MainMenu
+{
+    public class MainMenuViewModel
+    {
+        public DelegateCommand NewUserCommand { get; private set; }
+        public DelegateCommand RemoveUserCommand { get; private set; }
+        public DelegateCommand SaveCartInFileCommand { get; private set; }
+        public DelegateCommand LoadCartFromFileCommand { get; private set; }
+        public DelegateCommand ExportExcelFileCommand { get; private set; }
+
+        private MainViewModel _mainViewModel;
+
+        public MainMenuViewModel(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+
+            NewUserCommand = new DelegateCommand(SignUpNewUserWindow);
+            RemoveUserCommand = new DelegateCommand(RemoveUserWindows);
+            SaveCartInFileCommand = new DelegateCommand(SaveCartInFile);
+            LoadCartFromFileCommand = new DelegateCommand(LoadCartFromFile);
+            ExportExcelFileCommand = new DelegateCommand(ExportExcelFile);
+        }
+
+        private void ExportExcelFile()
+        {
+            _mainViewModel.ExportExcelFile();
+        }
+
+        public void SaveCartInFile()
+        {
+            _mainViewModel.SaveCartInFile();
+        }
+
+        public void LoadCartFromFile()
+        {
+            _mainViewModel.LoadCartFromFile();
+        }
+
+        private void SignUpNewUserWindow()
+        {
+            var signUpWindow = new SignUpWindow();
+            signUpWindow.ShowDialog();
+        }
+
+        private void RemoveUserWindows()
+        {
+            var removeUserWindow = new RemoveWindow();
+            removeUserWindow.ShowDialog();
+        }
+
+    }
+}
